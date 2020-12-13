71e6878111227a68061750e5a6f520b3f019c9da
alanginger <alanginger@alangingerdemacbook-pro.local>
2020-12-13 23:22:10 +0800

    week08

---
+++ week08/[231]2的幂.kt
@@ -0,0 +1,31 @@
+//给定一个整数，编写一个函数来判断它是否是 2 的幂次方。 
+//
+// 示例 1: 
+//
+// 输入: 1
+//输出: true
+//解释: 20 = 1 
+//
+// 示例 2: 
+//
+// 输入: 16
+//输出: true
+//解释: 24 = 16 
+//
+// 示例 3: 
+//
+// 输入: 218
+//输出: false 
+// Related Topics 位运算 数学 
+// 👍 265 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    fun isPowerOfTwo(n: Int): Boolean {
+        if (n == 0) return false
+        val x = n.toLong()
+        return x and -x == x
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
